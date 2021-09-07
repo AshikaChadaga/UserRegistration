c34348680ecf6db0a19129159802355e5ec03dff
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-07 17:20:05 +0530

    REFACTORED: Method to check for valid email address

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -18,7 +18,7 @@ public class UserRegistration {
 		isFirstName = matcherObject.matches();
 
 		if (isFirstName)
-			System.out.println(firstName + " is an Valid First Name");
+			System.out.println(firstName + " is an Valid First Name\n");
 		else
 			System.err.println(firstName + " is an Invalid First Name");
 	}
@@ -35,9 +35,26 @@ public class UserRegistration {
 		isLastName = matcherObject.matches();
 
 		if (isLastName)
-			System.out.println(lastName + " is an Valid Last Name");
+			System.out.println(lastName + " is an Valid Last Name\n");
 		else
 			System.err.println(lastName + " is an Invalid Last Name");
+	}
+	
+	public static void checkValidEmail(String email){
+		
+		boolean isEmail;
+		String emailRegex = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
+		Pattern patternObject = Pattern.compile(emailRegex);
+		if (email == null) {
+            isEmail = false;
+        }
+        Matcher matcherObject = patternObject.matcher(email);
+        isEmail =  matcherObject.matches();
+        
+        if(isEmail)
+			System.out.println(email+" is a Valid Email address\n");
+		else
+			System.out.println(email+" is an Invalid Email address");
 	}
 
 	public static void main(String[] args) {
@@ -53,6 +70,10 @@ public class UserRegistration {
 		System.out.println("Enter Your Last Name");
 		String lastName = scannerObject.next();
 		checkValidLastName(lastName);
+		
+		System.out.println("Enter Your Email Address");
+		String email = scannerObject.next();
+		checkValidEmail(email);
 
 		scannerObject.close();
 

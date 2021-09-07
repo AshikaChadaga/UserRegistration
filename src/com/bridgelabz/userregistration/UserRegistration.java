7308be9b42d8555d752c5263d5e26cccbd4462d9
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-07 18:30:19 +0530

    REFACTORED: Method to check if password has atleast 1 uppercase

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -22,7 +22,7 @@ public class UserRegistration {
 		else
 			System.err.println(firstName + " is an Invalid First Name");
 	}
-	
+
 	public static void checkValidLastName(String lastName) {
 
 		boolean isLastName;
@@ -39,56 +39,56 @@ public class UserRegistration {
 		else
 			System.err.println(lastName + " is an Invalid Last Name");
 	}
-	
-	public static void checkValidEmail(String email){
-		
+
+	public static void checkValidEmail(String email) {
+
 		boolean isEmail;
 		String emailRegex = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
 		Pattern patternObject = Pattern.compile(emailRegex);
 		if (email == null) {
-            isEmail = false;
-        }
-        Matcher matcherObject = patternObject.matcher(email);
-        isEmail =  matcherObject.matches();
-        
-        if(isEmail)
-			System.out.println(email+" is a Valid Email address\n");
+			isEmail = false;
+		}
+		Matcher matcherObject = patternObject.matcher(email);
+		isEmail = matcherObject.matches();
+
+		if (isEmail)
+			System.out.println(email + " is a Valid Email address\n");
 		else
-			System.out.println(email+" is an Invalid Email address");
+			System.out.println(email + " is an Invalid Email address");
 	}
-	
-	public static void checkValidMobileNumber(String mobileNumber){
-		
+
+	public static void checkValidMobileNumber(String mobileNumber) {
+
 		boolean isMobileNumber;
 		String mobileRegex = "^[0-9]{2}\\s{1}[0-9]{10}$";
 		Pattern patternObject = Pattern.compile(mobileRegex);
 		if (mobileNumber == null) {
-            isMobileNumber = false;
-        }
-        Matcher matcherObject = patternObject.matcher(mobileNumber);
-        isMobileNumber =  matcherObject.matches();
-        
-        if(isMobileNumber)
-			System.out.println(mobileNumber+" is a Valid  Mobile Number\n");
+			isMobileNumber = false;
+		}
+		Matcher matcherObject = patternObject.matcher(mobileNumber);
+		isMobileNumber = matcherObject.matches();
+
+		if (isMobileNumber)
+			System.out.println(mobileNumber + " is a Valid  Mobile Number\n");
 		else
-			System.out.println(mobileNumber+" is an Invalid Mobile Number");
+			System.out.println(mobileNumber + " is an Invalid Mobile Number");
 	}
-	
-	public static void checkValidPassword(String password){
-		
+
+	public static void checkValidPassword(String password) {
+
 		boolean isPassword;
-		String passwordRegex = "^([a-zA-Z0-9]*[\\-\\!\\@\\#\\$\\%\\^\\&\\*\\(\\)\\_\\+]*){8,}$";
+		String passwordRegex = "^(?=.*[A-Z])([a-zA-Z0-9]*([@#$%^&-+=()])*).{8,}$";
 		Pattern patternObject = Pattern.compile(passwordRegex);
 		if (password == null) {
-            isPassword = false;
-        }
-        Matcher matcherObject = patternObject.matcher(password);
-        isPassword =  matcherObject.matches();
-        
-        if(isPassword)
-			System.out.println(password+" is a Valid  Password\n");
+			isPassword = false;
+		}
+		Matcher matcherObject = patternObject.matcher(password);
+		isPassword = matcherObject.matches();
+
+		if (isPassword)
+			System.out.println(password + " is a Valid  Password\n");
 		else
-			System.out.println(password+" is an Invalid Password");
+			System.out.println(password + " is an Invalid Password");
 	}
 
 	public static void main(String[] args) {
@@ -98,21 +98,21 @@ public class UserRegistration {
 		Scanner scannerObject = new Scanner(System.in);
 
 		System.out.println("Enter Your First Name");
-		String firstName = scannerObject.next();
+		String firstName = scannerObject.nextLine();
 		checkValidFirstName(firstName);
-		
+
 		System.out.println("Enter Your Last Name");
-		String lastName = scannerObject.next();
+		String lastName = scannerObject.nextLine();
 		checkValidLastName(lastName);
-		
+
 		System.out.println("Enter Your Email Address");
-		String email = scannerObject.next();
+		String email = scannerObject.nextLine();
 		checkValidEmail(email);
-		
+
 		System.out.println("Enter Your Mobile Number");
 		String mobileNumber = scannerObject.nextLine();
 		checkValidMobileNumber(mobileNumber);
-		
+
 		System.out.println("Enter Your Password");
 		String password = scannerObject.nextLine();
 		checkValidPassword(password);
